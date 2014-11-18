5cb908ab116b03f105c9ab42794a1e4187944a5f
Robin Clart <robin@omise.co>
2014-11-18 17:08:35 +0700

    Add create/update/destroy to transfers

--- lib/omise/transfer.rb
+++ lib/omise/transfer.rb
@@ -4,12 +4,24 @@ module Omise
   class Transfer < OmiseObject
     self.endpoint = "transfers"
 
+    def self.create(attributes = {})
+      new resource(location, attributes).post(attributes)
+    end
+
     def self.retrieve(id, attributes = {})
       new resource(location(id), attributes).get
     end
 
     def reload(attributes = {})
       assign_attributes resource(attributes).get
+    end
+
+    def update(attributes = {})
+      assign_attributes resource(attributes).patch(attributes)
+    end
+
+    def destroy(attributes = {})
+      assign_attributes resource(attributes).delete
     end
   end
 end
