dc6968174d174d82829de238bdc0656fa111b22b
Jon Randy <jonrandy@gmail.com>
2019-02-06 11:26:12 +0700

    Adds some tests

--- test/omise/test_capabilities.rb
+++ test/omise/test_capabilities.rb
@@ -3,13 +3,21 @@ require "support"
 class TestCapabilities < Omise::Test
   setup do
     @capabilities = Omise::Capabilities.retrieve
-    backends = @capabilities.payment_backends
-    p backends.select{ |b| b['type'] == 'installment' }
+    @payment_backends = @capabilities.payment_backends
   end
 
   def test_that_we_can_retrieve_the_account
     assert_instance_of Omise::Capabilities, @capabilities
     assert_equal "/capability", @capabilities.location
+  end
+
+  def test_we_have_payment_backends
+    assert @payment_backends.is_a?(Array)
+    refute @payment_backends.empty?
+  end
+
+  def test_backends_have_ids
+    assert @payment_backends.all? { |backend| backend.key?("id") }
   end
 
 end
