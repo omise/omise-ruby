9499f4d8b86fc84222e414960a1f55a664623faf
Rezigned Kung <me@rezigned.com>
2019-02-07 11:56:26 +0700

    Add tests for sources api

--- lib/omise/source.rb
+++ lib/omise/source.rb
@@ -4,6 +4,10 @@ module Omise
   class Source < OmiseObject
     self.endpoint = "/sources"
 
+    def self.retrieve(id, attributes = {})
+      new resource(location(id), attributes).get(attributes)
+    end
+
     def self.create(attributes = {})
       new resource(location, attributes).post(attributes)
     end
