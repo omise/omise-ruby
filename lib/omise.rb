a83f43324f709da0aa6e247dac8cac028198e281
Robin Clart <robin@omise.co>
2019-11-19 15:31:08 +0700

    Each object its own line, sorted

--- lib/omise.rb
+++ lib/omise.rb
@@ -3,15 +3,36 @@ require "omise/configurator"
 
 module Omise
   LIB_PATH = File.expand_path("../", __FILE__)
+  OBJECTS = [
+    "account",
+    "balance",
+    "bank_account",
+    "capability",
+    "card",
+    "chain",
+    "charge",
+    "customer",
+    "dispute",
+    "document",
+    "event",
+    "forex",
+    "integration",
+    "link",
+    "occurrence",
+    "recipient",
+    "refund",
+    "schedule",
+    "search",
+    "source",
+    "token",
+    "transaction",
+    "transfer",
+  ]
 
   extend Configurator
 
   self.api_url   = "https://api.omise.co"
   self.vault_url = "https://vault.omise.co"
-
-  OBJECTS = %w[account balance bank_account capability card chain charge
-    customer dispute document event forex link integration occurrence recipient
-    refund search source schedule token transaction transfer]
 
   OBJECTS.each do |object|
     require "omise/#{object}"
